f331091823515ce91f3d30fe7cc0d008305d6577
Miguel Costa <miguelcosta.qn@gmail.com>
2023-10-19 15:15:04 +0100

    Improved overlay automatic size and positioning for real-time mode

--- overlay.py
+++ overlay.py
@@ -11,6 +11,10 @@ import settings
 class SpeedometerOverlay(QMainWindow):
     def __init__(self, screen, map_left, map_top, map_width):
         QMainWindow.__init__(self)
+        self.screen = screen
+        self.map_left = map_left
+        self.map_top = map_top
+        self.map_width = map_width
 
         self.setWindowFlags(
             Qt.WindowType.WindowStaysOnTopHint |
@@ -24,10 +28,11 @@ class SpeedometerOverlay(QMainWindow):
         self.widget.setStyleSheet('color: rgb'+str(settings.title_color)+';')
         self.layout = QVBoxLayout()
         self.layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
-        self.setStyleSheet("QLabel{font-size: 16pt;}")
+        self.setStyleSheet('QLabel{font-size: 16pt;}')
 
         # Close button
         self.close_button = QPushButton('X', self.widget)
+        self.close_button.setFixedSize(30, 30)
         self.close_button.clicked.connect(self.quit)
         self.close_button.setStyleSheet('font-size: 20pt;'
                                         'background-color: none;'
@@ -37,28 +42,20 @@ class SpeedometerOverlay(QMainWindow):
                                         'border-width: 2px;'
                                         'border-color: white;')
 
-        if settings.overlay_width is not None:
-            self.setGeometry(QRect(0, 0, settings.overlay_width, self.frameGeometry().height()))
-            self.layout.setContentsMargins(int((settings.overlay_width-100)/2),20,20,20)
-            self.close_button.setGeometry(settings.overlay_width - 30, 0, 30, 30)
-        else:
-            self.setGeometry(QRect(0, 0, map_width, self.frameGeometry().height()))
-            self.layout.setContentsMargins(int((map_width-100)/2),20,20,20)
-            self.close_button.setGeometry(map_width - 30, 0, 30, 30)
-
+        # Labels
         self.l_time = QLabel()
         self.l_total = QLabel('Total')
-        self.l_total.setStyleSheet("QLabel{font-size: 20pt; padding-top: 1em;}")
+        self.l_total.setStyleSheet('QLabel{font-size: 20pt; padding-top: 1em;}')
         self.l_speed = QLabel()
         self.l_avg_speed = QLabel()
         self.l_max_speed = QLabel()
         self.l_horizontal = QLabel('Horizontal')
-        self.l_horizontal.setStyleSheet("QLabel{font-size: 20pt; padding-top: 1em}")
+        self.l_horizontal.setStyleSheet('QLabel{font-size: 20pt; padding-top: 1em}')
         self.l_speed_h = QLabel()
         self.l_avg_speed_h = QLabel()
         self.l_max_speed_h = QLabel()
         self.l_vertical = QLabel('Vertical')
-        self.l_vertical.setStyleSheet("QLabel{font-size: 20pt; padding-top: 1em}")
+        self.l_vertical.setStyleSheet('QLabel{font-size: 20pt; padding-top: 1em}')
         self.l_speed_v = QLabel()
         self.l_avg_speed_v = QLabel()
         self.l_max_speed_v = QLabel()
@@ -80,21 +77,22 @@ class SpeedometerOverlay(QMainWindow):
         self.widget.setLayout(self.layout)
         self.setCentralWidget(self.widget)
 
-        self.screen = screen
-        # self.center()
-        self.move_to_pos(map_left, map_top)
+        self.layout.setContentsMargins(40,40,40,40)
 
-
-    def paintEvent(self, event=None):
-        painter = QPainter(self)
-        painter.setOpacity(0.2)
-        painter.setBrush(QColor(0,0,0))
-        painter.setPen(QPen(QColor(0,0,0)))
-        painter.drawRect(self.rect())
+        if settings.overlay_width is not None:
+            self.setGeometry(QRect(0, 0, settings.overlay_width, self.frameGeometry().height()))
+            self.close_button.setGeometry(settings.overlay_width - 30, 0, 30, 30)
+            self.layout.setContentsMargins((settings.overlay_width-100)/2, 40, 40, 40)
+        else:
+            self.setGeometry(QRect(0, 0, (self.map_width/1.5), self.frameGeometry().height()))
+            self.close_button.setGeometry((self.map_width/1.5) - 30, 0, 30, 30)
+            self.layout.setContentsMargins(((self.map_width/1.5)-100)/2, 40, 40, 40)
+        
+        self.move_to_map()
 
 
     def update_labels(self, stats, color):
-        current_time = str(datetime.datetime.now().strftime("%H:%M:%S"))
+        current_time = str(datetime.datetime.now().strftime('%H:%M:%S'))
         self.l_time.setText('Time: '+current_time)
         self.l_speed.setStyleSheet('color: rgb'+str(color)+';')
         self.l_avg_speed.setStyleSheet('color: rgb'+str(color)+';')
@@ -117,17 +115,32 @@ class SpeedometerOverlay(QMainWindow):
         self.l_max_speed_v.setText(f'Max: {stats["vertical"]["Max"]*settings.units[0]:0.2f} {settings.units[1]}')
 
 
+    def paintEvent(self, event=None):
+        painter = QPainter(self)
+        painter.setOpacity(0.2)
+        painter.setBrush(QColor(0,0,0))
+        painter.setPen(QPen(QColor(0,0,0)))
+        painter.drawRoundedRect(self.rect(), 10, 10)
+
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.close_button.setGeometry(self.widget.width() - 30, 0, 30, 30)
+        self.move_to_map()
+
+
     def center(self):
         geo = self.frameGeometry()
         geo.moveCenter(self.screen.geometry().center())
         self.move(geo.topLeft())
 
 
-    def move_to_pos(self, left, top):
+    def move_to_map(self):
         geo = self.frameGeometry()
         geo.moveCenter(self.screen.geometry().center())
         height = self.frameGeometry().height()
-        pos = QPoint(left, top) + QPoint(0, -height)
+        width = self.frameGeometry().width()
+        pos = QPoint(self.map_left, self.map_top) + QPoint(self.map_width/2 - width/2, -height)
         self.move(pos)
 
             
